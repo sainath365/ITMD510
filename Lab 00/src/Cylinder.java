243a1f5784199a309f557ea264b7e2cff432ae2e
Cdoherty95 <cdoherty@hawk.iit.edu>
2018-01-09 20:20:09 -0600

    finished lab 00

---
+++ Lab 00/src/Cylinder.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Cylinder
+{
+  public static void main(String args[])
+  {
+    // introduce a Scanner class object
+    Scanner sc = new Scanner(System.in);
+
+    // declare and initialize the variables
+    double height = 0, radius = 0, volume = 0, area = 0;
+    String strName = "";
+
+    // greet the program user	
+    System.out.println("Welcome to the Volume Program!");
+    // prompt user for their name
+    System.out.println("please enter your name");
+    // read the user name
+    strName = sc.nextLine();
+    //display the name back to the user
+    System.out.println("hello " + strName);
+	     
+    // input: assign values to the variables	
+    System.out.print("Please enter the radius. ");
+    radius = sc.nextDouble(); 
+    System.out.print("Please enter the height. ");
+    height = sc.nextDouble();
+ 
+    
+    // process: compute the required quantity
+    volume = 3.1416 * radius * radius * height;
+    area = 2 * 3.1416 * radius * height + 2 * 3.1416 * radius * radius;
+    
+    // output: display the output to the user
+    System.out.print("The volume of the cylinder is: ");
+    System.out.print(volume);
+    System.out.println(" cubic length units ");
+    
+ // output: display the output to the user
+    System.out.print("The surface area of the cylinder is: ");
+    System.out.print(area);
+    System.out.println(" square length units");
+    
+    // dismiss the Scanner class object
+    sc.close();
+  }
+}
